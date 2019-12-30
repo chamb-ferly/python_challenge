3c3e137e7a48b996dfa286e7f528c399b64b39e9
chamb-ferly <chamb-ferly@users.noreply.github.com>
2019-12-29 21:06:41 -0700

    PyBank

---
+++ PyBank/main.py
@@ -0,0 +1,45 @@
+
+#PyBank
+
+import csv
+
+with open("budget_data.csv") as csvfile:
+    pybank_reader = csv.reader(csvfile, delimiter=',')
+    print(pybank_reader)
+
+    pybank_header = next(pybank_reader)
+    print(pybank_header)
+
+    for row in pybank_reader:
+        print(row)
+
+
+#The total number of months included in the dataset
+total_months = 0
+with open("budget_data.csv") as csvfile:
+    pybank_reader = csv.reader(csvfile, delimiter=',')
+
+    for row in pybank_reader:
+        total_months += 1
+
+print(f"Total Months: {total_months -1}")
+
+# The net total amount of "Profit/Losses" over the entire period
+net_profit = 0
+profit_loss_index = 1
+
+with open("budget_data.csv") as csvfile:
+    pybank_reader = csv.reader(csvfile, delimiter=',')
+    if csv.Sniffer().has_header:
+        next(pybank_reader)
+    for row in pybank_reader:
+        float_net = float(row[profit_loss_index])
+        net_profit += float_net
+
+print(f"Total: ${net_profit}")
+
+# The average of the changes in "Profit/Losses" over the entire period
+  
+# The greatest increase in profits (date and amount) over the entire period
+
+# The greatest decrease in losses (date and amount) over the entire period
